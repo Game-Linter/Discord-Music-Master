ad4d1f0069aa75085da973c8fcf1e773c874e080
Mohamed Belkamel <belkamelmohamed@gmail.com>
2021-01-18 11:19:30 +0100

    types

--- src/@types/lyrics-parse/index.d.ts
+++ src/@types/lyrics-parse/index.d.ts
@@ -1,1 +1,5 @@
-declare module 'lyrics-parse';
+declare module 'lyrics-parse' {
+    import { Author } from 'ytdl-core';
+    function lyricsParse(title: string, author: Author): Promise<string>;
+    export = lyricsParse;
+}
--- src/utils/get-lyrics.ts
+++ src/utils/get-lyrics.ts
@@ -1,5 +1,5 @@
 import lyricsParse from 'lyrics-parse';
-import { getInfo } from 'ytdl-core';
+import { Author, getInfo } from 'ytdl-core';
 import { SPOTIFY_URI } from '../core/get-data-youtube';
 import { getSpotifyTrack } from './get-spotify-track';
 import { getAccessToken } from './get-token';
@@ -7,11 +7,14 @@ import { getAccessToken } from './get-token';
 export const getLyrics = async (url: string) => {
     if (url.startsWith(SPOTIFY_URI)) {
         const { search } = await getSpotifyTrack(await getAccessToken(), url);
-        const [title, author] = search.split(' ');
+        const [title, author] = search.split(' ') as [string, Author];
         return (await lyricsParse(title, author)).slice(0, 1980) as string;
     }
     const [title, author] = await getInfo(url).then((res) => {
-        return [res.videoDetails.title, res.videoDetails.author];
+        return [res.videoDetails.title, res.videoDetails.author] as [
+            string,
+            Author,
+        ];
     });
     const lyrics = (await lyricsParse(title, author)) as string;
     if (lyrics) {
