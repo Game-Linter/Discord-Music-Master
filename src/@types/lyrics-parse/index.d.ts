d8036795c8fda1d8d6372bdc9442828c5621df78
Mohamed Belkamel <belkamelmohamed@gmail.com>
2021-01-18 17:11:44 +0100

    fix: removed regex

--- src/@types/lyrics-parse/index.d.ts
+++ src/@types/lyrics-parse/index.d.ts
@@ -1,4 +1,20 @@
-declare module 'lyrics-parse' {
+/*
+ *    Copyright (C) 2021 Mohamed Belkamel
+ *
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU Affero General Public License as published
+ *   by the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU Affero General Public License for more details.
+ *
+ *   You should have received a copy of the GNU Affero General Public License
+ *   along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ */ declare module 'lyrics-parse' {
     import { Author } from 'ytdl-core';
     function lyricsParse(title: string, author: Author): Promise<string>;
     export = lyricsParse;
