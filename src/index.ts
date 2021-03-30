e41cd3f810df7b6945c2f5688ebe350c165ecde2
Mohamed Belkamel <belkamelmohamed@gmail.com>
2021-03-30 12:08:36 +0100

    currently playing

--- src/index.ts
+++ src/index.ts
@@ -18,6 +18,7 @@
 
 import { Message } from 'discord.js';
 import _ from 'lodash';
+import { getInfo } from 'ytdl-core';
 import { DiscordServer } from './core/discordServer';
 import { getData, SPOTIFY_URI } from './core/get-data-youtube';
 import { getCommand } from './core/getCommand';
@@ -442,11 +443,30 @@ const messageHandler = async (message: Message) => {
                     return message.channel.send('No channel on bot');
                 }
                 (async () => {
-                    const { title } = await getData(
+                    const { title, url } = await getData(
                         servers[id].getQueue[0],
                         message,
                     );
-                    return message.channel.send(`Now playing, ${title}`);
+                    let Author;
+                    if (Array.isArray(url)) {
+                        const { author } = await getInfo(url[0]).then((res) => {
+                            return {
+                                author: res.videoDetails.author.name,
+                            };
+                        });
+                        Author = author;
+                        // getTitleYoutube;
+                    } else {
+                        const { author } = await getInfo(url).then((res) => {
+                            return {
+                                author: res.videoDetails.author.name,
+                            };
+                        });
+                        Author = author;
+                    }
+                    return message.channel.send(
+                        `Currently playing, ${title} by ${Author}`,
+                    );
                 })();
                 break;
             case 'autoplay':
