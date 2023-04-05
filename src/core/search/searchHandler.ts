0abe37d46476f27d0c1e4d97624159e45844afa2
Mohamed Belkamel <belkamelmohamed@gmail.com>
2023-04-05 03:11:49 +0000

    ci: added workflow docker build

--- src/core/search/searchHandler.ts
+++ src/core/search/searchHandler.ts
@@ -2,7 +2,7 @@ import ytsr from 'ytsr';
 
 export class SearchHandler {
     async handle(query: string) {
-        const { url, title } = await ytsr(`${query} - Full video`, {
+        const { url, title } = await ytsr(query, {
             limit: 1,
             pages: 1,
         }).then((res) => res.items[0] as any);
