0dafdfb6f558c6d7422f41d1875257f8c4e1822b
Mohamed Belkamel <belkamelmohamed@gmail.com>
2023-04-06 04:29:39 +0000

    refactor: restored quit command

---
+++ src/commands/quit.command.ts
@@ -0,0 +1,39 @@
+import {
+    ChatInputCommandInteraction,
+    CacheType,
+    SlashCommandBuilder,
+} from 'discord.js';
+import PlayManager from '../core/PlayManager';
+import { Command } from './abstract/command.abstract';
+
+class Quit extends Command {
+    _data: any;
+
+    constructor() {
+        super();
+        this._data = new SlashCommandBuilder()
+            .setName('quit')
+            .setDescription('Quit the bot from the voice channel');
+    }
+
+    execute(
+        interaction: ChatInputCommandInteraction<CacheType>,
+    ): Promise<void> {
+        interaction.reply('Bye bye!');
+
+        const connectionState = PlayManager.getConnectionState(
+            interaction.guildId!,
+        );
+
+        if (!connectionState) return Promise.resolve();
+
+        connectionState.subscription.player.stop();
+        connectionState.subscription.connection.destroy();
+
+        PlayManager.deleteConnectionState(interaction.guildId!);
+
+        return Promise.resolve();
+    }
+}
+
+export default Quit;
