a7c7b1d64e6916249547f687c230684a5c174d8d
Mohamed Belkamel <belkamelmohamed@gmail.com>
2023-04-07 09:53:22 +0000

    feat: added support for youtube playlists

--- src/core/youtube/YoutubeHandler.ts
+++ src/core/youtube/YoutubeHandler.ts
@@ -1,11 +1,27 @@
 import { getInfo } from 'ytdl-core-discord';
 import { ResultUrl, UrlHandler } from '../abstract/UrlHandler';
+import ytpl from 'ytpl';
+import { Result } from '../QueryHandler';
 
 export class YoutubeHandler extends UrlHandler {
     public static readonly YOUTUBE_URI = 'https://www.youtube.com/watch?v=';
 
-    public async handleUrl(url: string): Promise<ResultUrl | null> {
+    public async handleUrl(url: string): Promise<Result> {
         const parsedUrl = new URL(url);
+
+        const playlistId = parsedUrl.searchParams.get('list');
+
+        if (playlistId) {
+            const playlist = await ytpl(playlistId);
+
+            const videoList = playlist.items.map((item) => ({
+                title: item.title,
+                url: `${YoutubeHandler.YOUTUBE_URI}${item.id}`,
+            }));
+
+            return videoList;
+        }
+
         const videoId = parsedUrl.searchParams.get('v');
 
         if (!videoId) return null;
