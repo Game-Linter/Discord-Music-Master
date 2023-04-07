6ed2d065407aef7454ad9bb9d5fef194bb09c49a
Mohamed Belkamel <belkamelmohamed@gmail.com>
2023-04-07 14:03:46 +0000

    feat: added load/save commands

---
+++ src/commands/load.command.ts
@@ -0,0 +1,102 @@
+import { getVoiceConnection, joinVoiceChannel } from '@discordjs/voice';
+import {
+    CacheType,
+    ChatInputCommandInteraction,
+    SlashCommandBuilder,
+} from 'discord.js';
+import PlayManager from '../core/PlayManager';
+import queryHandler from '../core/QueryHandler';
+import { GuildPlaylist } from '../persistence/models/Playlist';
+import { Command } from './abstract/command.abstract';
+
+class LoadCommand extends Command {
+    _data: any;
+
+    constructor() {
+        super();
+        this._data = new SlashCommandBuilder()
+            .setName('load')
+            .setDescription('loads a saved track/playlist from this server')
+            .addStringOption((option) =>
+                option
+                    .setName('name')
+                    .setDescription('The name of the playlist or track to load')
+                    .setRequired(true),
+            );
+    }
+
+    async execute(
+        interaction: ChatInputCommandInteraction<CacheType>,
+    ): Promise<void> {
+        await interaction.deferReply({
+            ephemeral: true,
+        });
+
+        const voiceChannel = await interaction.guild?.members
+            .fetch(interaction.member?.user.id!)
+            .then((member) => member.voice.channelId);
+
+        if (!voiceChannel) {
+            interaction.editReply({
+                content:
+                    'You need to be in a voice channel to use this command!',
+            });
+            return;
+        }
+
+        let voiceConnection = getVoiceConnection(interaction.guildId!);
+
+        if (!voiceConnection) {
+            voiceConnection = joinVoiceChannel({
+                channelId: voiceChannel,
+                guildId: interaction.guildId!,
+                adapterCreator: interaction.guild?.voiceAdapterCreator!,
+            });
+        }
+
+        const name = interaction.options.getString('name', true);
+
+        const guildPlaylists =
+            (await GuildPlaylist.get<Map<string, string>>(
+                interaction.guildId!,
+            )) || new Map<string, string>();
+
+        const playlist = guildPlaylists.get(name);
+
+        if (!playlist) {
+            interaction.editReply({
+                content: `No playlist with the name ${name} found`,
+            });
+
+            return;
+        }
+
+        const result = await queryHandler.handle(playlist);
+
+        if (!result) {
+            await interaction.editReply({
+                content: 'Link no longer valid!',
+            });
+        }
+
+        interaction.editReply({
+            content: `Loading ${name}`,
+        });
+
+        const played = await PlayManager.enqueueAudio(result, voiceConnection);
+
+        if (played?.action === 'play') {
+            interaction.editReply({
+                content: `Playing ${name}`,
+            });
+        } else {
+            interaction.editReply({
+                content: `Added ${name} to queue`,
+            });
+        }
+
+        return;
+    }
+}
+
+export default LoadCommand;
---
+++ src/commands/save.command.ts
@@ -0,0 +1,80 @@
+import {
+    CacheType,
+    ChatInputCommandInteraction,
+    SlashCommandBuilder,
+} from 'discord.js';
+import { GuildPlaylist } from '../persistence/models/Playlist';
+import { Command } from './abstract/command.abstract';
+
+class Save extends Command {
+    _data: any;
+
+    constructor() {
+        super();
+        this._data = new SlashCommandBuilder()
+            .setName('save')
+            .setDescription('Saves a track/playlist to this server with a name')
+            .addStringOption((option) =>
+                option
+                    .setName('name')
+                    .setDescription('The track to save')
+                    .setRequired(true),
+            )
+            .addStringOption((option) =>
+                option
+                    .setName('track/playlist')
+                    .setDescription('The link to save')
+                    .setRequired(true),
+            );
+    }
+
+    async execute(
+        interaction: ChatInputCommandInteraction<CacheType>,
+    ): Promise<void> {
+        const { name, link } = {
+            name: interaction.options.getString('name', true),
+            link: interaction.options.getString('track/playlist', true),
+        };
+
+        let guildPlaylists = await GuildPlaylist.get<GuildPlaylist>(
+            interaction.guildId!,
+        );
+
+        if (!guildPlaylists) {
+            guildPlaylists = new GuildPlaylist(interaction.guildId!);
+
+            guildPlaylists.value = new Map<string, string>();
+
+            guildPlaylists.value.set(name, link);
+
+            await guildPlaylists.save();
+        }
+
+        const playlists =
+            (await guildPlaylists.get()) || new Map<string, string>();
+
+        try {
+            playlists.set(name, link);
+
+            guildPlaylists.value = playlists;
+
+            await guildPlaylists.save();
+
+            interaction.reply({
+                content: `Saved ${name} to ${interaction.guildId}`,
+                ephemeral: true,
+            });
+        } catch (error: any) {
+            interaction.reply({
+                content: 'Failed to save',
+                ephemeral: true,
+            });
+
+            console.error(error.message);
+        }
+
+        return;
+    }
+}
+
+export default Save;
